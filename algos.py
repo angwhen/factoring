b49006cc4387e99ce4b697ff0515a879918ab5c7
angwhen <angelmwen@gmail.com>
2019-01-09 18:01:45 -0500

    diff squares

--- algos.py
+++ algos.py
@@ -23,6 +23,7 @@ def gcd(A,B):
         A = temp
     return A, steps
 
+# steps is # of MOD operations
 def trial_division(N,when_to_stop=None):
     primes_list = get_primes()
     m, pi, p, steps = N, 0, 2, 0
@@ -44,8 +45,50 @@ def trial_division(N,when_to_stop=None):
             num_unfactored = 1
     return factors,steps,num_unfactored
 
+def fermats_diff_of_squares_helper(N):
+    if N%2 == 0:
+        raise ValueError("Must be given odd N")
+    x = int(math.floor(math.sqrt(N)))
+    t = 2*x+1
+    r = x*x - N
+    steps = 0
+    while (not is_square(r)):
+        r += t
+        t += 2 
+        steps +=1
+    x = (t-1)/2
+    y = int(math.sqrt(r))
+    return x-y, x+y, steps
+
+# steps is # of checking for is_square
 def fermats_diff_of_squares(N):
-    return [],0,0
+    primes_list = get_primes()
+    factors = []
+    steps = 0
+
+    while N % 2 == 0:
+        steps +=1
+        factors.append(2)
+        N = N/2
+    
+    to_check = []
+    if N in primes_list:
+        factors.append(N)
+    elif N != 1:
+        x,y,helper_steps = fermats_diff_of_squares_helper(N)
+        to_check.append(x)
+        to_check.append(y)
+        steps += helper_steps
+    while len(to_check) != 0:
+        N = to_check.pop(0)
+        if N in primes_list:
+            factors.append(N)
+        elif N != 1:
+            x,y,helper_steps = fermats_diff_of_squares_helper(N)
+            to_check.append(x)
+            to_check.append(y)
+            steps += helper_steps
+    return factors,steps,0
 
 def harts_one_line_helper(N,L=-1):
     if L == -1:
@@ -63,17 +106,18 @@ def harts_one_line_helper(N,L=-1):
     steps += gcd_steps
     return f1, f2, steps
 
+# steps is # of MOD operations
 def harts_one_line(N):
     # check if N sqare
     primes_list = get_primes()
-    factors = []
-    steps = 0
+    factors = [N]
+    steps,num_unfactored = 0,1
     if not is_square(N):
         when_to_stop = math.pow(N,1.0/3.0)
         factors,steps,num_unfactored = trial_division(N, when_to_stop)
-        
     if num_unfactored == 1: #not possible in trial division to have more unfactored
         N = factors[-1]
+        factors = factors[:-1]
         #do harts one_line
         to_check = [N]
         leftover_factors = []
--- testing.py
+++ testing.py
@@ -1,14 +1,20 @@
 import algos
 
-success = False
+print algos.fermats_diff_of_squares_helper(27)
+print algos.fermats_diff_of_squares_helper(9)
 
+print algos.fermats_diff_of_squares(27)
+
+success = True
 for N in xrange(5,100):
+    print N 
     trial_factors,trial_steps,trial_left = algos.trial_division(N)
-    my_factors,my_steps,my_left = algos.harts_one_line(N)
+    my_factors,my_steps,my_left = algos.fermats_diff_of_squares(N)
     my_factors.sort()
     if trial_factors != my_factors:
         print "FAILURE at N = ", N
         print trial_factors, my_factors
+        success = False
         break
 
 if success:
